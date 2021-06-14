6d080526644556f3d3909f517e0c6e64b15a1712
pthomas0309 <preston.thomas355@gmail.com>
2021-06-14 12:26:29 -0500

    success page resets the reducer and navigates to the beginning of the form

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,10 +1,35 @@
+// source useHistory
+import {useHistory} from 'react-router-dom';
+
+// source useDispatch
+import {useDispatch} from 'react-redux';
+
 function Success() {
+    
+    // make useHistory available as history
+    const history = useHistory();
+
+    // make useDispatch available as dispatch
+    const dispatch = useDispatch();
+
+    // function to handle starting the feedback form over again
+    const handleRestart = () => {
+
+        // reset state to the default
+        dispatch({
+            type: 'RESET'
+        });
+
+        // navigate to the beginning of the form
+        history.push('/')
+    }
+
     return (
         <div>
             <h3>You Did It!</h3>
             <h4><strong>Thanks for the submission</strong></h4>
             <p>Would you like to start a new feedback form?</p>
-            <button>Start New Feedback</button>
+            <button onClick={handleRestart}>Start New Feedback</button>
         </div>
     )
 }
