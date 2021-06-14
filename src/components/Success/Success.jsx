755781b199433f54be4ef73148400055098cfaa3
pthomas0309 <preston.thomas355@gmail.com>
2021-06-14 12:12:29 -0500

    success page skeleton

---
+++ src/components/Success/Success.jsx
@@ -0,0 +1,12 @@
+function Success() {
+    return (
+        <div>
+            <h3>You Did It!</h3>
+            <h4><strong>Thanks for the submission</strong></h4>
+            <p>Would you like to start a new feedback form?</p>
+            <button>Start New Feedback</button>
+        </div>
+    )
+}
+
+export default Success;
